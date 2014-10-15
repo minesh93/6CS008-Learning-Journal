757d75ff0b545f5e66453e46b5462169d54fee40
Minesh Mitha <Minesh_mitha@hotmail.com>
2014-10-16 00:25:09 +0100

    Basics Added
    I have added two file indicated that I have understood the basics of
    python in terms of making basic variables and printing items on to the
    display.

---
+++ code/basic variables.py
@@ -0,0 +1,15 @@
+numberOne = 15 #-- no need to declare variable type
+
+stringOne = "A String Yo"
+
+numberTwo = 10 
+
+numberThree = numberOne + numberTwo #-- Adding variables
+
+#-- Most of the syntax feels somewhat similar to JavaScript!
+
+
+print stringOne
+
+print numberThree
+
---
+++ code/hello.py
@@ -0,0 +1,1 @@
+print 'hello world' #Best Hello world ever!
