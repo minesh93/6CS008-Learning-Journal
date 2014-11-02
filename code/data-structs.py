c82e789c569eab1bdc6d8d7aa8577b4381ecde0c
Minesh Mitha <Minesh_mitha@hotmail.com>
2014-11-02 17:37:41 +0000

    Added Notebook 3
    Notebook 3 is about dictionaries and supporting code in data-structs.py
    to test that they can also have functions stored in them.

--- code/data-structs.py
+++ code/data-structs.py
@@ -56,6 +56,19 @@ print jsonLikeStruct['testItem']
 #-- Not like an object though
 # print jsonLikeStruct.testItem
 
-#-- Aaaand sublime has an inbuilt python console!
+#-- Assign a function to dictionaries
 
+def printHello():  
+	print 'hello from a function'
+
+jsonLikeStruct['funct'] = printHello
+
+#-- Call the function that is stored in the dictionary 
+
+jsonLikeStruct['funct']()
+
+#-- Dictionaries can be used like JSON object but they are alot quicker than arrays to search through stuff
+#-- They also replicate hash maps in Java.
+#-- Due to the key value method of searching through items which means 
+#-- they are a lot faster to work with.
 
