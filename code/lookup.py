c977250258b9f1f1ca7ddd8d3d6e507b70aa0582
Minesh Mitha <Minesh_mitha@hotmail.com>
2014-11-03 16:29:06 +0000

    Added Lecture 4

---
+++ code/lookup.py
@@ -0,0 +1,6 @@
+lookup = {}
+lookup['rock'] = {'rock':'draw', 'paper':'lose', 'scissors':'win','lizard':'win','spock':'lose'}
+lookup['paper'] = {'rock':'win', 'paper':'draw', 'scissors':'lose','lizard':'lose''spock':'win'}
+lookup['scissors'] = {'rock':'lose', 'paper':'win', 'scissors':'draw','lizard':'win','spock':'lose'}
+lookup['lizard'] = {'rock':'draw', 'paper':'lose', 'scissors':'win', 'lizard':'draw','spock':'win'}
+lookup['spock'] = {'rock':'draw', 'paper':'lose', 'scissors':'win', 'lizard':'lose','spock':'draw'}
